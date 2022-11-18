741c5079fb987634479ef0cb5a8c534f82c622c8
psam1017 <psam1017@naver.com>
2022-11-18 16:12:00 +0900

    FIRST COMMIT

---
+++ WebContent/test.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>테스트 페이지입니다</title>
+</head>
+<body>
+	<h1>데이터 전송 테스트. 인사해보세요!</h1>
+	<form action="World" method="post">
+		<input type="text" name="greet" value="">
+		<input type="submit">
+	</form>
+</body>
+</html>
---
+++ src/test/World.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/World")
+public class World extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+  public World() {
+      super();
+  }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+
+		PrintWriter out = response.getWriter();
+		String greet = request.getParameter("greet");
+
+		out.print("<html><body><h1>" + greet + "</h1>");
+		out.close();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
