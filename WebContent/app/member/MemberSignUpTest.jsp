7784cfff7b9b5c04c53cc2b29d2a9c6fc21cec40
hamiwood <hamiwood@naver.com>
2022-12-09 18:12:21 +0900

    shn finish signup and withdraw

--- WebContent/app/member/MemberSignUpTest.jsp
+++ WebContent/app/member/MemberSignUpTest.jsp
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="김지혜">
+    <meta name="author" content="손하늘">
     <meta name="description" content="이 세상의 모든 꼼수를 다루는 꼼수닷컴입니다.">
     <title>회원가입 : 꼼수닷컴</title>
 </head>
@@ -22,8 +22,8 @@
                 <li class="signUpLi">비밀번호 확인 <input type="password"></li>
                 <li class="signUpLi">닉네임 <input type="text" name="nickname"></li>
                 <li class="signUpLi">이름 <input type="text" name="name"></li>
-                <li class="signUpLi">성별: 남성<input type="radio" name="sex" value="남성" checked>
-                        	여성<input type="radio" name="sex" value="여성"></li>
+                <li class="signUpLi">성별: 남성<input type="radio" name="sex" value="M" checked>
+                        	여성<input type="radio" name="sex" value="F"></li>
                 <li class="signUpLi">생년월일 <input type="date" name="birthDate"></li>
                 <li class="signUpLi">통신사 <select name="telecomValue">
                     <option value="KT">KT</option>
--- src/com/ggomsu/app/member/service/MemberSignUpOk.java
+++ src/com/ggomsu/app/member/service/MemberSignUpOk.java
@@ -18,21 +18,31 @@ public class MemberSignUpOk implements Action{
 		MemberVO vo = new MemberVO();
 		MemberDAO dao = new MemberDAO();
 		ActionForward forward = new ActionForward();
+		System.out.println("실행2");
 		
 		// getParameter의 안에 String 값은 jsp에서 만든 name의 값과 같아야 한다!
+		System.out.println("실행100");
 		vo.setEmail(req.getParameter("email"));
+		System.out.println("실행200");
 		vo.setPassword(req.getParameter("password"));
-		vo.setNaverKey(req.getParameter("naverKey"));
-		vo.setKakaoKey(req.getParameter("kakaoKey"));
-		vo.setGoogleKey(req.getParameter("googleKey"));
+		//vo.setNaverKey(req.getParameter("naverKey"));
+		//vo.setKakaoKey(req.getParameter("kakaoKey"));
+		//vo.setGoogleKey(req.getParameter("googleKey"));
+		System.out.println("실행300");
 		vo.setNickname(req.getParameter("nickname"));
+		System.out.println("실행400");
 		vo.setName(req.getParameter("name"));
+		System.out.println("실행500");
 		vo.setBirthDate(req.getParameter("birthDate"));
+		System.out.println("실행600");
 		vo.setSex(req.getParameter("sex"));
+		System.out.println("실행700");
 		vo.setTelecomValue(req.getParameter("telecomValue"));
+		System.out.println("실행800");
 		vo.setContact(req.getParameter("contact"));
 		vo.setZipcode(req.getParameter("zipcode"));
 		vo.setAddress(req.getParameter("address"));
+		System.out.println("실행900");
 		vo.setAddressDetail(req.getParameter("addressDetail"));
 		vo.setProfileImageUrl(req.getParameter("profileImageUrl"));
 		
@@ -44,7 +54,7 @@ public class MemberSignUpOk implements Action{
 		
 		forward.setForward(false);
 		System.out.println(req.getContextPath());
-		forward.setPath(req.getContextPath() + "/app/Member/MemberSignUpOk.jsp");
+		forward.setPath(req.getContextPath() + "/app/member/MemberSignUpOk.jsp");
 		
 		System.out.println("실행3!");
 		
